4224a0ca9a8b20b416bd0405847cf47d4c9a972d
xulun <lusinga@126.com>
2022-08-22 22:18:34 +0800

    update

---
+++ addbinary.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"container/list"
+	"strings"
+)
+
+func addCarry(a byte, carry1 bool, carry bool) (byte, bool) {
+	//fmt.Println("Add Carry:", a, carry)
+	if carry {
+		if a == '0' {
+			return '1', carry1
+		} else {
+			return '0', true
+		}
+	} else {
+		return a, carry1
+	}
+}
+
+func addOne(a byte, b byte, carry bool) (byte, bool) {
+	//fmt.Println(a, b, carry)
+	if a == '0' && b == '0' {
+		return addCarry('0', false, carry)
+	} else if a == '0' && b == '1' {
+		return addCarry('1', false, carry)
+	} else if a == '1' && b == '0' {
+		return addCarry('1', false, carry)
+	} else if a == '1' && b == '1' {
+		return addCarry('0', true, carry)
+	} else {
+		panic("Invalid input")
+	}
+}
+
+func addBinary(a string, b string) string {
+	listA := list.New()
+	listB := list.New()
+	listR := list.New()
+	for i := len(a) - 1; i >= 0; i-- {
+		listA.PushBack(a[i])
+	}
+	for i := len(b) - 1; i >= 0; i-- {
+		listB.PushBack(b[i])
+	}
+	carry := false
+
+	//len_a := listA.Len()
+
+	item_a := listA.Front()
+	item_b := listB.Front()
+	for {
+		if item_a == nil && item_b == nil {
+			break
+		} else {
+			var val1 byte = '0'
+			var val2 byte = '0'
+			if item_a != nil {
+				val1 = item_a.Value.(byte)
+				item_a = item_a.Next()
+			}
+			if item_b != nil {
+				val2 = item_b.Value.(byte)
+				item_b = item_b.Next()
+			}
+			val, carry1 := addOne(val1, val2, carry)
+			carry = carry1
+			//fmt.Println(val, carry)
+			listR.PushBack(val)
+		}
+	}
+	if carry {
+		var bytec byte = '1'
+		listR.PushBack(bytec)
+	}
+
+	var build strings.Builder
+
+	for i := listR.Back(); i != nil; i = i.Prev() {
+		//fmt.Println(i.Value)
+		build.WriteByte(i.Value.(byte))
+	}
+
+	result := build.String()
+	return result
+}
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello")
+	list1 := list.New()
+	list1.PushBack(1)
+	list1.PushBack(2)
+	fmt.Println(list1)
+	//fmt.Println("Hello")
+	val := addBinary("11", "1")
+	fmt.Println(val)
 }
